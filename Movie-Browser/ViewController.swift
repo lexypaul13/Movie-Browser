0e52422dd39a692192fdbd2ddc4c6ac1e6d7700e
Alex Paul <lexypaul13@yahoo.com>
2021-02-19 20:36:50 -0500

    Initial Commit

---
+++ Movie-Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-Browser
+//
+//  Created by Alex Paul on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
